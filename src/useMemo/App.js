19c06178db52d59f8c247fad2d469702f9b1c77d
Mia <tjwlfif@gmail.com>
2023-01-01 21:29:33 +0900

    Add: useMemo 예제2

--- src/useMemo/App.js
+++ src/useMemo/App.js
@@ -1,44 +1,36 @@
-import React, { useMemo, useState } from "react";
-
-const hardCalculate = (number) => {
-  console.log("어려운 계산!");
-  // 생각하는 시간
-  return number + 10000;
-};
-
-const easyCalculate = (number) => {
-  console.log("짱 쉬운 계산!");
-  return number + 1;
-};
+import React, { useEffect, useMemo, useState } from "react";
 
 function App() {
-  const [hardNumber, setHardNumber] = useState(1);
-  const [easyNumber, setEasyNumber] = useState(1);
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
 
-  // App 컴포넌트가 렌더링 될 때마다 hardCalculate 함수를 반복실행
-  // const hardSum = hardCalculate(hardNumber);
-  const hardSum = useMemo(() => {
-    return hardCalculate(hardNumber);
-  }, [hardNumber]);
-  const easySum = easyCalculate(easyNumber);
+  // location이 객체 타입으로 변하면 렌더링 될 때마다 location의 메모리 주소가 변경되기 때문에 계속 값이 변하는 것으로 인지
+  // const location = {
+  //   country: isKorea ? "한국" : "외국",
+  // };
+
+  const location = useMemo(() => {
+    return {
+      country: isKorea ? "한국" : "외국",
+    };
+  }, [isKorea]);
+
+  useEffect(() => {
+    console.log("useEffect 호출");
+  }, [location]);
 
   return (
     <div>
-      <h3>어려운 계산기</h3>
+      <h2>하루에 몇끼 먹어요?</h2>
       <input
         type="number"
-        value={hardNumber}
-        onChange={(e) => setHardNumber(parseInt(e.target.value))}
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
       />
-      <span> + 10000 = {hardSum}</span>
-
-      <h3>쉬운 계산기</h3>
-      <input
-        type="number"
-        value={easyNumber}
-        onChange={(e) => setEasyNumber(parseInt(e.target.value))}
-      />
-      <span> + 1 = {easySum}</span>
+      <hr />
+      <h2>어느 나라에 있어요?</h2>
+      <p>나라: {location.country}</p>
+      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
     </div>
   );
 }
