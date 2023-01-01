b099e3782ef0529f701ef5decd870f9d45636762
Mia <tjwlfif@gmail.com>
2023-01-01 19:19:41 +0900

    Add: useRef_1: 변수와 Ref의 차이

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import UseRef_1 from "./useRef/useRef_1";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<UseRef_1 />);
